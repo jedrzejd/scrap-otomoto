4196c633702a108a9d22e7cfecbd435878fd8f36
Jędrzej Dudzicz <jedrzejdudzicz.com>
2021-02-07 12:10:24 +0100

    adding get_cars_names.py

---
+++ get_cars_names.py
@@ -0,0 +1,20 @@
+import requests, bs4
+
+
+path = 'https://www.otomoto.pl/osobowe/acura/'
+
+res = requests.get(path)
+res.raise_for_status()
+
+carSoup = bs4.BeautifulSoup(res.text, features="lxml")
+
+section = carSoup.find('select', title="Marka pojazdu")
+
+models = section.find_all('option')
+car_model = []
+for model in models[1:]:
+    name = model.text.strip().split()[:-1]
+    name = "-".join(name).lower()
+    print(name)
+    car_model.append(name)
+
