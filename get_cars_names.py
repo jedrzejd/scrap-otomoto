ad51165c0b36419bc25afa6a9a4563dd15597810
Jędrzej Dudzicz <jedrzejdudzicz@gmail.com>
2021-03-02 11:16:45 +0100

    Fixing get_cars_names.py. Updating Requirements.txt

--- get_cars_names.py
+++ get_cars_names.py
@@ -12,10 +12,17 @@ section = carSoup.find('select', title="Marka pojazdu")
 
 models = section.find_all('option')
 car_model = []
+
+car_exceptions = {'warszawa':'marka_warszawa'}
+
 for model in models[1:]:
     name = model.text.strip().split()[:-1]
     name = "-".join(name).lower()
     name = unidecode.unidecode(name)
+    if name in car_exceptions:
+        name = car_exceptions[name]
     print(name)
     car_model.append(name)
 
+File_car_names = open('car_models.txt', 'w')
+File_car_names.writelines(car + '\n' for car in car_model)
